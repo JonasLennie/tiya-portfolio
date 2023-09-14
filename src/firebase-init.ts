3c5e573cc5ac5a18305e38f74918dea397c09bb5
JonasLennie3 <jonas.lennie.21@ucl.ac.uk>
2023-09-14 18:18:20 +0100

    Firebase setup

---
+++ src/firebase-init.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDs27B0JXFftL35-weh16dT6Ct9aTX9CHU",
+  authDomain: "tiya-portfolio.firebaseapp.com",
+  projectId: "tiya-portfolio",
+  storageBucket: "tiya-portfolio.appspot.com",
+  messagingSenderId: "1043943834182",
+  appId: "1:1043943834182:web:9e4cebaff82ad9925d8047"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
